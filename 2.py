447058e0b83bbad7f0ff3401816ae24c1a431413
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-04-04 17:05:41 -0500

    Se añade el desarrollo de 2.py funcionando y compilando

---
+++ 2.py
@@ -0,0 +1,35 @@
+#Desarrollo Ejercicio 2
+#Antes de empezar vamos a poner un texto que nos funcione para todos los ejemplos que voy a poner
+a="pedro Felipe Gómez"
+#La primera función que quiero mostrar es "swapcase()" el cual permite invertir el case (mayúsculas a minúsculas y viceversa) todas las palabras 
+#se utiliza mediante declarandolo en una variable e imprimiéndola. Esto aplica para todas las funciones
+c=a.swapcase()
+print(c)
+#La segunda funcion a utilizar es "isdecimal()" el cual me dice si lo ingresado es decimal o no mediante True o False
+d=a.isdecimal()
+print(d)
+#La tercera función a utilizar es "upper()" el cual convierte todo lo que está en minúscula en mayúscula
+e=a.upper()
+print(e)
+#La cuarta función a utilizar es "isnumeric()" el cual se encarga de decirme si un string tiene solamente carácteres numéricos mediante True o False
+f=a.isnumeric()
+print(f)
+#La quinta función es "capitalize()" la cual capitaliza la primera letra de un string
+g=a.capitalize()
+print(g)
+#La sexta función es "isspace()" el cual me dice si el string solamente contiene carácteres con espacio
+h=a.isspace()
+print(h)
+#La séptima función es "lower()" el cual se encarga de convertir todo a minúscula
+j=a.lower()
+print(j)
+#La octava función se llama "istitle()" que sirve para saber si un string tiene las propiedades de un título
+k=a.istitle()
+print(k)
+#La novena función es "isupper()" la cual dice si todos los carácteres son mayúsculas
+m=a.isupper()
+print(m)
+#La novena función es "islower()" que sirve para decir si todos los carácteres estan en minúscula
+n=a.islower() 
+print(n)
+#Desarrollado por Pedro Gómez / ID:000396221
