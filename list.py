e185aa0eba825030a4b0a1cd8a11c28d8793d070
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-04-04 18:24:50 -0500

    Se añade el archivo list.py funcionando y compilando

---
+++ list.py
@@ -0,0 +1,16 @@
+#Listas
+#Primero vamos a crear una lista con diversas palabras
+lista = ["camilo","andres","kenneth"]
+lista2 = [123,456,789]
+#Primera función
+lista2.reverse()
+print(lista2)
+#Segunda función
+print(lista[2])
+#Tercera función
+print(len(lista2))
+#Cuarta función
+print(lista[0:1])
+#Quinta función
+print(lista2[2:])
+#Desarrollado por Pedro Gómez / ID:000396221
