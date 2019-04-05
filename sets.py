8006c176cc79a67acdd102a3c3a0eae665e25f32
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-04-05 07:26:29 -0500

    Se actualiza el archivo sets.py

---
+++ sets.py
@@ -0,0 +1,18 @@
+#Sets
+#Se crea un set para el ejemplo
+mi_set={23,34,56}
+#Primera función
+print(mi_set)
+#Segunda función
+mi_set.add(2)
+print(mi_set)
+#Tercera función
+mi_set.update([4,5],{43,56})
+print(mi_set)
+#Cuarta función
+mi_set.discard(23)
+print(mi_set)
+#Quinta función
+mi_set.remove(34)
+print(mi_set)
+#Desarrollado por Pedro Gómez / ID:000396221
