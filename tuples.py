614c4da19ab70168833dacf22b4e0f9dc97f3c41
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-04-05 08:20:18 -0500

    Se añade la actualización de tuples.py

---
+++ tuples.py
@@ -0,0 +1,15 @@
+#Tuples
+#Primero creamos los tuples
+mi_tuple = 23,56,67,456,78
+#Primera función
+print(mi_tuple)
+#Segunda función
+print(mi_tuple[2])
+#Tercera función
+print(mi_tuple[-1])
+#Cuarta función
+print(mi_tuple[2:4])
+#Quinta función
+a=len(mi_tuple)
+print(a)
+#Desarrollado por Pedro Gómez / ID:000396221
