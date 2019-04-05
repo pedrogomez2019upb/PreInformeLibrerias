4f166058cb3efc61510c84d3dcefb07692c6ab5b
pedrogomez2019upb <47305995+pedrogomez2019upb@users.noreply.github.com>
2019-04-05 07:50:54 -0500

    Se actualiza dictorionary.py

--- dictionary.py
+++ dictionary.py
@@ -1,2 +1,19 @@
 #Directorios
 #Primero creamos el directorio
+mi_directorio={1: 'hola' , 2: 'andres'}
+mi_directorio2={1: 'one', 2:'two'}
+#Primera funcion
+print(mi_directorio[1])
+#Segunda función
+mi_directorio[2]='camilo'
+print(mi_directorio[2])
+#Tercera función
+del mi_directorio [1]
+print(mi_directorio)
+#Cuarta función
+mi_directorio.update(mi_directorio2)
+print(mi_directorio)
+#Quinta función
+mi_directorio.clear()
+print("Mi_directorio= {}".format(mi_directorio))
+#Desarrollado por Pedro Gómeze / ID:000396221
